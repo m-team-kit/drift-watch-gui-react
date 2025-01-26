7f3d3db3a06f8c514d001bb6d6142b47d9fe9ec6
christophe <christophe.misc+git@protonmail.ch>
2025-01-27 00:31:01 +0100

    feat: basic drifts filtering

---
+++ src/components/ButtonBadge.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+import { X } from 'lucide-react';
+import { type PropsWithChildren } from 'react';
+
+const ButtonBadge = ({ onClick, children }: PropsWithChildren<{ onClick: () => void }>) => (
+  <button
+    className={cn(
+      'inline-flex items-center rounded-xl border px-2.5 py-0.5 text-xs font-semibold text-foreground transition-colors focus:outline-none focus:ring-1 focus:ring-ring focus:ring-offset-1',
+      'group',
+    )}
+    onClick={onClick}
+  >
+    {children}
+
+    <X className="group-hover:text-destructive" />
+  </button>
+);
+
+export default ButtonBadge;
--- src/components/experiment/Drifts.tsx
+++ src/components/experiment/Drifts.tsx
@@ -1,11 +1,67 @@
 import experimentIdDriftSearchPost from '@/api/functions/experimentIdDriftSearchPost';
 import { type Experiment } from '@/api/models';
+import ButtonBadge from '@/components/ButtonBadge';
 import { driftsColumns } from '@/components/driftsTable';
 import Paginate from '@/components/Paginate';
+import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import getQueryPagination from '@/lib/getQueryPagination';
 import { useQuery } from '@tanstack/react-query';
-import { type FC, useMemo, useState } from 'react';
+import { Plus } from 'lucide-react';
+import { type Dispatch, type FC, type SetStateAction, useMemo, useState } from 'react';
+
+const FilterInput = ({
+  filter,
+  setFilter,
+}: {
+  filter: string[];
+  setFilter: Dispatch<SetStateAction<string[]>>;
+}) => {
+  const [newFilter, setNewFilter] = useState('');
+
+  return (
+    <div className="mb-2 flex justify-center">
+      <div className="flex items-center grow max-w-[80ch]">
+        <Label htmlFor="new-tag" className="self-center me-2">
+          Filter tag
+        </Label>
+        <Input
+          className="max-w-[14ch]"
+          onChange={(e) => setNewFilter(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setFilter([...filter, newFilter]);
+              setNewFilter('');
+            }
+          }}
+          value={newFilter}
+        />
+        <Button
+          className="ms-2"
+          onClick={() => {
+            setFilter([...filter, newFilter]);
+            setNewFilter('');
+          }}
+          size="sm"
+        >
+          <Plus /> <span className="sr-only">Add Tag</span>
+        </Button>
+        <div className="ms-2 flex gap-1">
+          {filter.map((f) => (
+            <ButtonBadge
+              key={f}
+              onClick={() => setFilter((filters) => filters.filter((filter) => filter !== f))}
+            >
+              {f}
+            </ButtonBadge>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 type DriftsProps = {
   experiment: Experiment;
@@ -14,15 +70,23 @@ const Drifts: FC<DriftsProps> = ({ experiment }) => {
   const [page, setPage] = useState(1);
   const columns = useMemo(() => driftsColumns(experiment.id), [experiment.id]);
 
+  const [filter, setFilter] = useState<string[]>([]);
+
   const drifts = useQuery({
-    queryKey: ['experimentDrift', experiment.id, page],
+    queryKey: ['experimentDrift', experiment.id, page, filter.join(',')],
     queryFn: () =>
       experimentIdDriftSearchPost({
         params: {
           experiment_id: experiment.id,
           page,
         },
-        body: {},
+        body: {
+          ...(filter.length > 0 && {
+            tags: {
+              $all: filter,
+            },
+          }),
+        },
         config: {
           basePath: 'https://drift-watch.dev.ai4eosc.eu/api/latest',
         },
@@ -31,32 +95,58 @@ const Drifts: FC<DriftsProps> = ({ experiment }) => {
 
   const pagination = getQueryPagination(drifts);
 
+  // TODO: deduplicate the <FilterInput filter={filter} setFilter={setFilter} /> in all the returns
+
   if (drifts.isLoading || drifts.isPending) {
-    return <div>Loading...</div>;
+    return (
+      <>
+        <FilterInput filter={filter} setFilter={setFilter} />
+        <div>Loading...</div>
+      </>
+    );
   }
 
   if (drifts.isError) {
-    return <div>Error: {drifts.error.message}</div>;
+    return (
+      <>
+        <FilterInput filter={filter} setFilter={setFilter} />
+        <div>Error: {drifts.error.message}</div>
+      </>
+    );
   }
 
   if (drifts.data.status !== 200) {
     switch (drifts.data.status) {
       case -1:
-        return <div>Network error</div>;
+        return (
+          <>
+            <FilterInput filter={filter} setFilter={setFilter} />
+            <div>Network error</div>
+          </>
+        );
       case 422:
-        return <div>Malformed query</div>;
+        return (
+          <>
+            <FilterInput filter={filter} setFilter={setFilter} />
+            <div>Malformed query</div>
+          </>
+        );
       case 'default':
         return (
-          <div>
-            Unknown error
-            {drifts.data.data.message}
-          </div>
+          <>
+            <FilterInput filter={filter} setFilter={setFilter} />
+            <div>
+              Unknown error
+              {drifts.data.data.message}
+            </div>
+          </>
         );
     }
   }
 
   return (
     <>
+      <FilterInput filter={filter} setFilter={setFilter} />
       <DataTable columns={columns} data={drifts.data.data} />
       {pagination.status === 'valid' ? (
         <Paginate page={page} setPage={setPage} pagination={pagination.data} />
