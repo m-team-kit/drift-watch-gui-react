c26e9aaff313c57eb4f5ae8ee72adf4714f7a416
Christophe <christophe.misc+git@protonmail.ch>
2025-03-18 13:00:21 +0100

    fix: add delete permission button

--- src/components/permissionsTable.tsx
+++ src/components/permissionsTable.tsx
@@ -3,6 +3,7 @@ import { type Experiment, type Permission } from '@/api/models/index';
 import { useAuth } from '@/components/AuthContext';
 import columnSortButton from '@/components/columnSortButton';
 import { useExperiment } from '@/components/experiment/experimentContext';
+import { Button } from '@/components/ui/button';
 import {
   Select,
   SelectContent,
@@ -16,11 +17,13 @@ import { API_BASEPATH } from '@/lib/env';
 import useAllowedToEdit from '@/lib/useAllowedToEdit';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { type ColumnDef } from '@tanstack/react-table';
+import { Trash } from 'lucide-react';
 
 type ChangePermissionCellProps = {
   permission: Required<Experiment>['permissions'][number];
+  className?: string;
 };
-const ChangePermisionCell = ({ permission }: ChangePermissionCellProps) => {
+const ChangePermisionCell = ({ permission, className }: ChangePermissionCellProps) => {
   const experiment = useExperiment();
   const allowed = useAllowedToEdit(experiment);
   const auth = useAuth();
@@ -67,7 +70,7 @@ const ChangePermisionCell = ({ permission }: ChangePermissionCellProps) => {
       }}
       defaultValue={permission.level}
     >
-      <SelectTrigger>
+      <SelectTrigger className={className}>
         <SelectValue placeholder="permission" />
       </SelectTrigger>
       <SelectContent>
@@ -81,6 +84,53 @@ const ChangePermisionCell = ({ permission }: ChangePermissionCellProps) => {
     </Select>
   );
 };
+type DeletePermissionProps = {
+  permission: Required<Experiment>['permissions'][number];
+  className?: string;
+};
+const DeletePermission = ({ permission, className }: DeletePermissionProps) => {
+  const experiment = useExperiment();
+  const allowed = useAllowedToEdit(experiment);
+  const auth = useAuth();
+  const queryClient = useQueryClient();
+
+  const delet = useMutation({
+    mutationFn: async () => {
+      const response = await experimentIdPut({
+        params: { experiment_id: experiment.id },
+        body: {
+          name: experiment.name,
+          description: experiment.description,
+          public: experiment.public,
+          permissions: [
+            ...(experiment.permissions?.filter((p) => permission.entity !== p.entity) ?? []),
+          ],
+          // TODO: generator support read-only fields as omitted for requests
+        } as Experiment,
+        config: {
+          basePath: API_BASEPATH,
+          auth: {
+            bearer: auth.status === 'logged-in' ? auth.auth.token : undefined,
+          },
+        },
+      });
+      await queryClient.invalidateQueries({
+        queryKey: ['experiment', experiment.id],
+      });
+      return response;
+    },
+  });
+
+  if (!allowed) {
+    return null;
+  }
+
+  return (
+    <Button onClick={() => delet.mutate()} variant="destructive" className={className}>
+      <Trash aria-label="Delete" />
+    </Button>
+  );
+};
 
 export const permissionsColumns: ColumnDef<Permission>[] = [
   {
@@ -90,6 +140,11 @@ export const permissionsColumns: ColumnDef<Permission>[] = [
   {
     accessorKey: 'level',
     header: 'Level',
-    cell: (ctx) => <ChangePermisionCell permission={ctx.row.original} />,
+    cell: (ctx) => (
+      <div className="flex flex-row">
+        <ChangePermisionCell permission={ctx.row.original} />
+        <DeletePermission permission={ctx.row.original} className="ms-2" />
+      </div>
+    ),
   },
 ];
