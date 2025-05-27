688e26ef58fa8ed3db7baf3cbed0f259587437c8
Borja Esteban <boressan@outlook.com>
2025-05-27 09:24:46 +0200

    feat: add navigation links for documentation, GitHub, and issues in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,12 @@ import LoginButton from '@/components/LoginButton';
 import { Link } from '@tanstack/react-router';
 import Logo from '../assets/drift_watch-logo.svg';
 import styles from './Header.module.scss';
+import { FaGithub, FaExclamationCircle } from 'react-icons/fa';
+import { SiReadthedocs } from 'react-icons/si';
+
+const docsUrl = import.meta.env.VITE_DOCS_URL;
+const githubUrl = import.meta.env.VITE_GITHUB_URL;
+const issuesUrl = `${githubUrl}/issues`;
 
 const Header = () => {
   return (
@@ -15,6 +21,20 @@ const Header = () => {
             style={{ height: '50px' }}
           />
           Experiments
+        </Link>
+      </div>
+      <div className={styles.nav_links}>
+        <Link to={docsUrl} className={styles.nav_link} activeOptions={{ exact: true }}>
+          <SiReadthedocs className={styles.icon} />
+          <span className={styles.linkText}>Documentation</span>
+        </Link>
+        <Link to={githubUrl} className={styles.nav_link} activeOptions={{ exact: true }}>
+          <FaGithub className={styles.icon} />
+          <span className={styles.linkText}>GitHub</span>
+        </Link>
+        <Link to={issuesUrl} className={styles.nav_link} activeOptions={{ exact: true }}>
+          <FaExclamationCircle className={styles.icon} />
+          <span className={styles.linkText}>Issues</span>
         </Link>
       </div>
       <div className={styles.login}>
